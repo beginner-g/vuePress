6eb3cf0cb27c05b9a2b4e6b6487c5bb43dafa201
beginner-g <969925523@qq.com>
2019-11-19 09:56:13 +0800

    start

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'gs的前端之旅',
+  description: 'Personal Website',
+  serviceWorker: true, // 是否开启 PWA
+  base: '/vuePress/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    nav:[ // 导航栏配置
+      {text: 'vue', link: '/vue/' },
+    ],
+    sidebar:[
+      {
+        title: 'vue', // 侧边栏名称
+        collapsable: true, // 可折叠
+        children: [
+          '/vue/', // 你的md文件地址
+        ]
+      }
+    ]
+  }
+};
---
+++ docs/main.md
@@ -0,0 +1,10 @@
+---
+title: '方向'
+sidebar: 'auto'
+sidebarDepth: 4
+---
+## 一、前端工程师路线
+
+![路线](https://www.maomin.club/data/lux.jpg)
+
+当你看到这幅图时，一切才刚刚开始。
